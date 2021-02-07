07b24c6ff7b7c73b907a6ad0e58b12ac98cf2576
Момчил Миланов <deadman_kula@abv.bg>
2021-02-07 16:49:01 +0200

    Add project files.

---
+++ 01. Clock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
